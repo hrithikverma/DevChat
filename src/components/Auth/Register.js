ea6295b875c089a1f034e4e265ed8b607134f1ee
hrithikverma <niranjanverma906@gmail.com>
2021-06-02 23:19:41 +0530

    Added Registration page

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -17,7 +17,50 @@ class Register extends React.Component {
     email: "",
     password: "",
     passwordConfirmation: "",
+    errors: [],
   };
+
+  isFormValid = () => {
+    let errors = [];
+    let error;
+    if (this.isFormEmpty(this.state)) {
+      error = { message: "Fill in all fields" };
+      this.setState({ errors: errors.concat(error) });
+      return false;
+    } else if (!this.isPasswordValid(this.state)) {
+      error = { message: "Password is invalid" };
+      this.setState({ errors: errors.concat(error) });
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  isFormEmpty = ({ username, email, password, passwordConfirmation }) => {
+    return (
+      !username.length ||
+      !email.length ||
+      !password.length ||
+      !passwordConfirmation.length
+    );
+  };
+
+  isPasswordValid = ({ password, passwordConfirmation }) => {
+    if (
+      password.length < 6 ||
+      passwordConfirmation.length < 6 ||
+      password !== passwordConfirmation
+    ) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  displayErrors = (errors) =>
+    errors.map((error, i) => {
+      return <p key={i}>{error.message}</p>;
+    });
 
   handleChange = (event) => {
     this.setState({ [event.target.name]: event.target.value });
@@ -25,25 +68,29 @@ class Register extends React.Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(this.state.email, this.state.password)
-      .then((createdUser) => {
-        console.log(createdUser);
-      });
+    if (this.isFormValid()) {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then((createdUser) => {
+          console.log(createdUser);
+        })
+        .catch((err) => console.log(err));
+    }
   };
 
   render() {
-    const { username, email, password, passwordConfirmation } = this.state;
+    const { username, email, password, passwordConfirmation, errors } =
+      this.state;
 
     return (
-      <Grid textAlign="center" verticalAlign="middle" className="app">
+      <Grid textAlign="center" verticalAlign="middle" className="app register">
         <Grid.Column style={{ maxWidth: 450 }}>
-          <Header as="h2" icon color="orange" textAlign="center">
-            <Icon name="puzzle piece" color="orange" />
+          <Header as="h2" icon color="violet" textAlign="center">
+            <Icon name="puzzle piece" color="violet" />
             Register for DevChat
           </Header>
-          <Form onSubmit={this.handleSubmit} size="large">
+          <Form onSubmit={this.handleSubmit} size="large" autoComplete="off">
             <Segment stacked>
               <Form.Input
                 fluid
@@ -86,13 +133,16 @@ class Register extends React.Component {
                 type="password"
               />
 
-              <Button color="orange" fluid size="large">
+              <Button color="violet" fluid size="large">
                 Submit
               </Button>
             </Segment>
           </Form>
+          {errors.length > 0 && (
+            <Message error>{this.displayErrors(errors)}</Message>
+          )}
           <Message>
-            Already a user? <Link to="/login">Login</Link>{" "}
+            Already a user? <Link to="/login">Login</Link>
           </Message>
         </Grid.Column>
       </Grid>
