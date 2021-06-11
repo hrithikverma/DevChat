c75577ca7cc2ae86fc12e306e812bcb6fb8af781
hrithikverma <niranjanverma906@gmail.com>
2021-06-12 00:43:18 +0530

    Building out chat console

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
-import './App.css';
+import React from "react";
+import { Grid } from "semantic-ui-react";
+import { connect } from "react-redux";
+import "./App.css";
+import ColorPanel from "./ColorPanel/ColorPanel";
+import SidePanel from "./SidePanel/SidePanel";
+import Messages from "./Messages/Messages";
+import MetaPanel from "./MetaPanel/MetaPanel";
 
-function App() {
+const App = ({ currentUser }) => {
   return (
-    <div>
-      App
-    </div>
+    <Grid columns="equal" className="app">
+      <ColorPanel />
+      <SidePanel currentUser={currentUser} />
+      <Grid.Column style={{ marginLeft: 320 }}>
+        <Messages />
+      </Grid.Column>
+      <Grid.Column width={4}>
+        <MetaPanel />
+      </Grid.Column>
+    </Grid>
   );
-}
+};
 
-export default App;
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import { createStore } from "redux";
 import { Provider, connect } from "react-redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducers";
-import { setUser } from "./actions/index";
+import { setUser, clearUser } from "./actions/index";
 
 // global state
 const store = createStore(rootReducer, composeWithDevTools());
@@ -24,9 +24,12 @@ class Root extends React.Component {
   componentDidMount() {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
-        console.log(user);
+        // console.log(user);
         this.props.setUser(user);
         this.props.history.push("/");
+      } else {
+        this.props.history.push("/login");
+        this.props.clearUser();
       }
     });
   }
@@ -50,7 +53,9 @@ const mapStateFromProps = (state) => {
   };
 };
 
-const RootWithAuth = withRouter(connect(mapStateFromProps, { setUser })(Root));
+const RootWithAuth = withRouter(
+  connect(mapStateFromProps, { setUser, clearUser })(Root)
+);
 
 ReactDOM.render(
   <Provider store={store}>
