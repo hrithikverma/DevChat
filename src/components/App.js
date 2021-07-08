146ae644b877e1698c5c2ee1824dd2ea66060484
hrithikverma <niranjanverma906@gmail.com>
2021-07-08 16:35:56 +0530

    Attached Chosen Colors to User Data

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,10 @@ import MetaPanel from "./MetaPanel/MetaPanel";
 const App = ({ currentUser, currentChannel, isPrivateChannel, userPosts }) => {
   return (
     <Grid columns="equal" className="app">
-      <ColorPanel />
+      <ColorPanel
+        key={currentUser && currentUser.name}
+        currentUser={currentUser}
+      />
       <SidePanel
         key={currentUser && currentUser.uid}
         currentUser={currentUser}
@@ -29,7 +32,7 @@ const App = ({ currentUser, currentChannel, isPrivateChannel, userPosts }) => {
       </Grid.Column>
       <Grid.Column width={4}>
         <MetaPanel
-          key={currentChannel && currentChannel.id}
+          key={currentChannel && currentChannel.name}
           currentChannel={currentChannel}
           userPosts={userPosts}
           isPrivateChannel={isPrivateChannel}
--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -1,18 +1,38 @@
 import React from "react";
-import {
-  Sidebar,
-  Menu,
-  Divider,
-  Button,
-  Modal,
-  Icon,
-  Label,
-} from "semantic-ui-react";
+import firebase from "../../firebase";
+// prettier-ignore
+import { Sidebar, Menu, Divider, Button, Modal, Icon, Label, Segment } from "semantic-ui-react";
 import { SliderPicker } from "react-color";
 
 class ColorPanel extends React.Component {
   state = {
     modal: false,
+    primary: "",
+    secondary: "",
+    user: this.props.currentUser,
+    usersRef: firebase.database().ref("users"),
+  };
+
+  handleChangePrimary = (color) => this.setState({ primary: color.hex });
+
+  handleChangeSecondary = (color) => this.setState({ secondary: color.hex });
+
+  handleSaveColors = () => {
+    const { primary, secondary } = this.state;
+    if (primary && secondary) {
+      this.state.usersRef
+        .child(`${this.state.user.uid}/colors`)
+        .push()
+        .update({
+          primary,
+          secondary,
+        })
+        .then(() => {
+          console.log("Colors added");
+          this.closeModal();
+        })
+        .catch((err) => console.error(err));
+    }
   };
 
   openModal = () => this.setState({ modal: true });
@@ -20,7 +40,7 @@ class ColorPanel extends React.Component {
   closeModal = () => this.setState({ modal: false });
 
   render() {
-    const { modal } = this.state;
+    const { modal, primary, secondary } = this.state;
 
     return (
       <Sidebar
@@ -38,13 +58,23 @@ class ColorPanel extends React.Component {
         <Modal basic open={modal} onClose={this.closeModal}>
           <Modal.Header> Choose App Colors </Modal.Header>
           <Modal.Content>
-            <Label content="Primary Color" />
-            <SliderPicker />
-            <Label content="Secondary Color" />
-            <SliderPicker />
+            <Segment inverted>
+              <Label content="Primary Color" />
+              <SliderPicker
+                color={primary}
+                onChange={this.handleChangePrimary}
+              />
+            </Segment>
+            <Segment inverted>
+              <Label content="Secondary Color" />
+              <SliderPicker
+                color={secondary}
+                onChange={this.handleChangeSecondary}
+              />
+            </Segment>
           </Modal.Content>
           <Modal.Actions>
-            <Button color="green" inverted>
+            <Button color="green" inverted onClick={this.handleSaveColors}>
               <Icon name="checkmark" /> Save Colors
             </Button>
             <Button color="red" inverted onClick={this.closeModal}>
